5961ca58e2fc84a667b796f82f5b0858caeb9416
Kany Sy Boye <kanysy02@gmail.com>
2024-12-04 19:26:42 +0000

    Ajout et Modification de l exo

--- GestionEleve.java
+++ GestionEleve.java
@@ -1,84 +1,55 @@
-class GestionEleve {
-    private String nom;
-    private int id;
+public class GestionEleve{
 
-    public Personne(String nom, int id) {
-        this.nom = nom;
-        this.id = id;
-    }
+class Personne {
+    String nom;
+    String prenom;
+   String dateDeNaissance;
 
-    public String getNom() {
-        return nom;
-    }
-
-    public int getId() {
-        return id;
-    }
-}
-
-class Cours {
-    private String nom;
-    private int id;
-
-    public Cours(String nom, int id) {
-        this.nom = nom;
-        this.id = id;
-    }
-
-    public String getNom() {
-        return nom;
-    }
-
-    public int getId() {
-        return id;
-    }
+    // Constructeur 
 }
 
 class Eleve extends Personne {
-    private <Cours> cours;
-    private <Cours, Double> notes;
+    int matricule;
+    Note[] notes;
 
-    public Eleve(String nom, int id) {
-        super(nom, id);
-        this.cours = new note();
-        this.notes = new cours();
-    }
+    // Constructeur 
 }
 
-    public void ajouterCours(Cours cours) {
-        this.cours.add(cours);
-        System.out.println("Cours ajouté : " + cours.getNom());
-    }
-    public void ajouterNote(Cours cours, double note) {
-        if (this.cours.contains(cours)) {
-            notes.put(cours, note);
-            System.out.println("Note ajoutée pour le cours " + cours.getNom() + ": " + note);
-        } else {
-            System.out.println("L'élève n'est pas inscrit à ce cours.");
-        }
-    }
+class Professeur extends Personne {
+    String matiere;
 
+    // Constructeur
+}
+class Note {
+    int valeur;
+    Cours cours;
 
+    // Constructeur 
+}
 
+class Cours {
+    String intitule;
+Professeur professeur;
+    Eleve[] eleves;
 
-public class Gestion {
+    // Constructeur 
+}
+
+public class GestionAcademica {
+    static Eleve[] eleves = new Eleve[100]; 
+    static Cours[] cours = new Cours[100]; 
+    static int nbEleves = 0;
+    static int nbCours = 0;
+
     public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
         
-        Cours Français= new Cours("Français", 101);
-        Cours physique = new Cours("Physique", 102);
-
-        
-        Eleve eleve1 = new Eleve("Fatou", 1);
-
-        
-        eleve1.ajouterCours(Français);
-        eleve1.ajouterCours(physique);
-
-        eleve1.ajouterNote(Français, 15.5);
-        eleve1.ajouterNote(physique, 18.0);
+    }
 
     
-        eleve1.afficherCoursEtNotes();
+    public static void ajouterEleve() {
+        eleves[nbEleves] = new Eleve();
+        nbEleves++;
     }
 }
-
+}
